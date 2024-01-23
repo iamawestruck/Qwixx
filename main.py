c340d93c3da14f101d929c33dba1b650f2ccdd49
iamawestruck <eharrisonsoccer@gmail.com>
2024-01-23 15:24:10 -0800

    added first file

--- main.py
+++ main.py
@@ -2,15 +2,35 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+order = []
+
+class Board:
+    red = None
+    yellow = None
+    green = None
+    blue = None
+    penalty = None
+    def Board(self):
+        self.red=[]
+        self.yellow = []
+        self.green = []
+        self.blue = []
+        self.penalty = 0
+
+class Player:
+    board = None
+    def Player(self):
+        self.board = Board()
+class Game:
+    # name
+def playerCount(name):
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
+def diceRoll():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# First ask for number of players
+# Roll a dice for each player to determine who's first & the order
+#
